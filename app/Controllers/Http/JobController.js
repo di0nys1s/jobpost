9e0e0081f40965fb33da3bbcc74105fd433172c4
Burak Seyhan <seyhanburak9@gmail.com>
2020-03-23 13:45:38 +1100

    login and signup

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -1,6 +1,24 @@
-'use strict'
+/*jshint esversion: 6 */
+"use strict";
+
+const Job = use("App/Models/Job");
 
 class JobController {
+  async home({ view }) {
+    // Create a job
+    // const job = new Job();
+    // job.title = "My job title";
+    // job.link = "https://google.com";
+    // job.description = "My job description";
+
+    // await job.save();
+
+    // Fetch a job
+    const jobs = await Job.all();
+
+    // return jobs to index view with JSON format
+    return view.render("index", { jobs: jobs.toJSON() });
+  }
 }
 
-module.exports = JobController
+module.exports = JobController;
